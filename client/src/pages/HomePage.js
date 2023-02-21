f0ccc23d94ee10a2074b9f6b6666ad4449282617
youba idjis <youba.etud@gmail.com>
2023-02-20 23:51:05 -0800

    MVP seed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -19,7 +19,7 @@ function Homepage() {
             <img src={product.image_url} alt={product.name} className="w-full h-40 object-cover mb-4" />
             <h2 className="text-lg font-bold mb-2">{product.name}</h2>
             <p className="text-gray-500 mb-2">{product.description}</p>
-            <p className="text-lg font-bold">{product.price}</p>
+            <p className="text-lg font-bold">$ {product.price}</p>
           </div>
         ))}
       </div>
