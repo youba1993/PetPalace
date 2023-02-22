5556c8105a570deaf3de55ffc90cba8ac580206d
youba idjis <youba.etud@gmail.com>
2023-02-21 23:18:32 -0800

    HomePagerestyling

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
+  const [selectedCategory, setSelectedCategory] = useState(0)
   return (
-    <div className="p-8 container mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Welcome to PetPalace</h1>
-      <p className="text-gray-500 mb-4">Find the best deals on everything you need for your furry friend.</p>
+    <div className='p-8'>
+    <div className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl font-semibold mb-6">
+            Shop for your furry friend
+          </h2>
+          <p className="text-lg mb-4">
+            Discover our wide selection of pet products and accessories.
+          </p>
+        </div>
+      </div>
       <div className="flex justify-center">
         <Link to="/products" className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
           Shop Now
         </Link>
       </div>
+    </div>
     </div>
   );
 }
